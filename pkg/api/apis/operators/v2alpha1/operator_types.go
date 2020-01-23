e311c67b679d7c89d6374a84aac3e9958bcc8d6f
njhale <njohnhale@gmail.com>
2020-01-22 20:58:42 -0500

    feat(apis): add operator types

---
+++ pkg/api/apis/operators/v2alpha1/operator_types.go
@@ -0,0 +1,84 @@
+package v2alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// OperatorSpec is the specification of an operator.
+type OperatorSpec struct{}
+
+// OperatorStatus describes the observed state of an operator and its components.
+type OperatorStatus struct {
+	// Components describes resources that compose the operator.
+	// +optional
+	Components *Components `json:"components,omitempty"`
+}
+
+// ConditionType codifies a condition's type.
+type ConditionType string
+
+// Conditions represent the latest available observations of an object's state
+type Condition struct {
+	// Type of condition.
+	Type ConditionType `json:"type"`
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+	// The reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// A human readable message indicating details about the transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+	// Last time the condition was probed
+	// +optional
+	LastUpdateTime *metav1.Time `json:"lastUpdateTime,omitempty"`
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty"`
+}
+
+// Components tracks the resources that compose an operator.
+type Components struct {
+	// LabelSelector is a label query over a set of resources used to select the operator's components
+	LabelSelector *metav1.LabelSelector `json:"labelSelector"`
+	// Refs are a set of references to the operator's component resources, selected with LabelSelector.
+	// +optional
+	Refs []RichReference `json:"refs,omitempty"`
+}
+
+// RichReference is a reference to a resource, enriched with its status conditions.
+type RichReference struct {
+	*corev1.ObjectReference `json:",inline"`
+	// Conditions represents the latest state of the object.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +genclient
+// +genclient:nonNamespaced
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:storageversion
+
+// Operator represents an operator on the cluster.
+type Operator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OperatorSpec   `json:"spec,omitempty"`
+	Status OperatorStatus `json:"status,omitempty"`
+}
+
+// +genclient:nonNamespaced
+// +kubebuilder:object:root=true
+
+// OperatorList is a list of operator resources.
+type OperatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Operator `json:"items"`
+}
